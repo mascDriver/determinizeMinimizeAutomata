8898611327bf26c4a7a78f3220debefb3ec9a1c1
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2022-04-01 22:49:50 -0300

    refatoraçao do codigo, AFND sendo gerado com sucesso

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from itertools import chain
 
 from tabulate import tabulate
 
+from unicodedata import normalize
 
 def print_table(table, headers):
     print(tabulate(table, headers, tablefmt="fancy_grid"))
+
+
+def prepare_alphabet_machine():
+    formalize_data = [list(word) for word in words]
+    return list(dict.fromkeys(list(chain.from_iterable(formalize_data))))
 
 
 class Automata:
@@ -15,48 +21,45 @@ class Automata:
         self.epsilon = '\u03B5'
         self.alphabet = string.ascii_uppercase
         self.words = words
-        self.alphabet_machine = self.prepare_alphabet_machine()
+        self.alphabet_machine = prepare_alphabet_machine()
         self.state = [[self.symbol_initial] + [''] * len(self.alphabet_machine)]
         self.last_state = 0
-
-    def prepare_alphabet_machine(self):
-        formalize_data = [list(word) for word in words]
-        return list(dict.fromkeys(list(chain.from_iterable(formalize_data))))
 
     def get_state(self, position=0, **kwargs):
         return kwargs.get('state_final', '') + list(self.alphabet)[self.last_state - position]
 
-    def create_state(self, final=False, new=True):
-        state_final = ''
-        if final:
-            state_final = '*'
-
+    def create_state(self, new=True, **kwargs):
         if new:
-            self.state = self.state + [[self.get_state(state_final=state_final)] + [''] * len(self.alphabet_machine)]
+            self.state = self.state + [[self.get_state(state_final=kwargs.get('state_final', ''))] + [''] * len(self.alphabet_machine)]
             self.last_state += 1
 
     def automata_n_deteminize(self):
-        for key_word, word in enumerate(words):
+        for num_word, word in enumerate(words):
             len_word = len(word) - 1
-            for key_letter, letter in enumerate(word):
-                if self.state[0][self.alphabet_machine.index(letter) + 1] and key_letter == 0:
-                    self.state[0][self.alphabet_machine.index(letter) + 1] += list(self.alphabet)[self.last_state - 1]
+            for num_letter, letter in enumerate(word):
+                if num_letter == 0 and len_word:
+                    if num_word == 0:
+                        self.state[0][self.alphabet_machine.index(letter) + 1] += list(self.alphabet)[self.last_state]
+                        self.create_state()
+                    else:
+                        self.state[0][self.alphabet_machine.index(letter) + 1] += list(self.alphabet)[self.last_state-1]
+
                 else:
                     if len_word == 0:
                         self.state[self.last_state][self.alphabet_machine.index(letter) + 1] = list(self.alphabet)[
                             self.last_state - 1]
                         if not self.get_state(position=1, state_final='*') in chain(*self.state):
                             self.state[self.last_state][0] = '*' + self.state[self.last_state][0]
-                        if key_letter == 0:
-                            self.state[0][self.alphabet_machine.index(letter) + 1] = list(self.alphabet)[
+                        if num_letter == 0:
+                            self.state[0][self.alphabet_machine.index(letter) + 1] += list(self.alphabet)[
                                 self.last_state - 1]
                     else:
-                        if key_letter == len_word:
+                        if num_letter == len_word:
                             self.state[self.last_state][self.alphabet_machine.index(letter) + 1] = list(self.alphabet)[
                                 self.last_state]
-                            self.create_state(True)
+                            self.create_state(state_final='*')
                             try:
-                                if len(words[key_word + 1]) == 1:
+                                if len(words[num_word + 1]) == 1:
                                     self.create_state(new=False)
                             except IndexError:
                                 continue
@@ -67,14 +70,15 @@ class Automata:
                             self.create_state()
 
 
-# words = ['se', 'entao', 'senao', 'a', 'i', 'o', 'u']
+# words = ['se', 'entao', 'senao', 'a', 'e', 'i', 'o', 'u']
 words = []
-while(True):
+while True:
     command = input('Digite a palavra, para finalizar escreva "EOF": ')
     if command.lower() == 'eof':
         break
     else:
-        words.append(command)
+        words.append(normalize('NFKD', command).encode('ASCII', 'ignore').decode('ASCII').lower())
+
 aut = Automata(words=words)
 aut.automata_n_deteminize()
 print_table(aut.state, [aut.delta] + aut.alphabet_machine)
